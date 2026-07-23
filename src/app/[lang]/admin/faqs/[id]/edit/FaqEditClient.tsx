c826be0f5e7fbf4d56262c9024898d0e73ec1247
khbm0110 <khalidbm011@gmail.com>
2026-07-23 17:32:46 +0100

    Delete src directory

--- src/app/[lang]/admin/faqs/[id]/edit/FaqEditClient.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { updateFaq } from '@/app/actions/admin';
-import FaqForm from '@/components/admin/FaqForm';
-
-export default function FaqEditClient({ lang, faq }: { lang: string; faq: any }) {
-  return <FaqForm lang={lang} faq={faq} onSave={(data) => updateFaq(faq.id, data)} />;
-}
--- src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-// DENTORA-OS - ROOT LAYOUT
-// This is the root layout that wraps all other layouts
-
-import { ReactNode } from 'react';
-import './[lang]/globals.css';
-
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return children;
-}
